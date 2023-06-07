5afbd5fd3e69837372aec4afc947e34173ecfe32
emrearal <78971902+emrearal@users.noreply.github.com>
2023-06-07 21:55:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-ayg4mr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-ayg4mr)
---
+++ app/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <Page>
+    <ActionBar title="Düğme Örneği" />
+    <StackLayout>
+      <Button text="Arttır" @tap="increaseCounter" />
+      <Button text="Başka Bileşeni Aç" @tap="openChildComponent" />
+      <Label :text="counter.toString()" />
+      <Label :text="showChildComponent.toString()" />
+
+      <Label v-if="showChildComponent" text="Başka Bileşen" />
+      <Button
+        v-if="showChildComponent"
+        text="Kapat"
+        @tap="closeChildComponent()"
+      />
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+      showChildComponent: false,
+    };
+  },
+  methods: {
+    increaseCounter() {
+      this.counter += 1;
+    },
+    openChildComponent() {
+      this.showChildComponent = true;
+    },
+    closeChildComponent() {
+      this.showChildComponent = false;
+    },
+  },
+};
+</script>
