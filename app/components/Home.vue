a93576b10ea006e55c2b575292766796d323f6b5
emrearal <78971902+emrearal@users.noreply.github.com>
2023-06-07 22:50:29 +0300

    1 file modified

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <Page>
-    <ActionBar title="Düğme Örneği" />
+    <ActionBar title="Düğme Örneği">
+      <NavigationButton
+        text="Go back"
+        android.systemIcon="ic_menu_back"
+        @tap="goBack"
+      />
+    </ActionBar>
+
     <StackLayout>
+      <Button text="Action Dialog'u Aç" @tap="openActionDialog" />
       <Button text="Arttır" @tap="increaseCounter" />
       <Button text="Başka Bileşeni Aç" @tap="openChildComponent" />
       <Label :text="counter.toString()" />
@@ -18,14 +26,29 @@
 </template>
 
 <script>
+import { ActionDialog } from '@nativescript/core';
 export default {
   data() {
     return {
       counter: 0,
       showChildComponent: false,
+      listOfItems: ['anan', 'baban', 'kardeşin'],
     };
   },
   methods: {
+    openActionDialog() {
+      const options = {
+        title: 'Action Dialog',
+        message: 'Bu bir action dialog örneğidir.',
+        actions: ['Seçenek 1', 'Seçenek 2', 'Seçenek 3'],
+        cancelButtonText: 'İptal',
+      };
+
+      ActionDialog.show(options).then((result) => {
+        console.log('Seçilen indeks:', result);
+      });
+    },
+
     increaseCounter() {
       this.counter += 1;
     },
